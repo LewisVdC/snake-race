0de33a08914970cb7deb6eef9a532f13f9021b3e
Lewis Van den Cruyce <lewis.vandencruyce@leerling.kov.be>
2026-05-04 11:24:56 +0200

    Add project files.

---
+++ actual snake race/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace actual_snake_race
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1315, 1071);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(1500, 12);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 23);
+            this.startbutton.TabIndex = 1;
+            this.startbutton.Text = "start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1500, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "gok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1472, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "op snake nummer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1472, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "voor zoveel euro";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1475, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1475, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1333, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "geld: 100";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1587, 1095);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ actual snake race/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace actual_snake_race
+{
+    public partial class Form1 : Form
+    {
+        Brush body = new SolidBrush(Color.Green);
+        Brush head = new SolidBrush(Color.DarkGreen);
+        Brush checker1 = new SolidBrush(Color.White);
+        Brush checker2 = new SolidBrush(Color.Black);
+        Graphics paper;
+        int[] snakepos = new int[5];
+        Random rand = new Random();
+        int counter = 0;
+        int gamble = 0;
+        int money = 100;
+        int gambleamount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            paper = pictureBox1.CreateGraphics();
+        }
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            if (gamble == 0)
+            {
+                MessageBox.Show("eerst gamblen 🤑🤑🤑🤑🤑🤑");
+                return;
+            }
+            timer1.Start();
+            draw();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter++;
+
+            if (counter >= 50)
+            {
+                int index = rand.Next(0, 5);
+
+                snakepos[index] -= 55;
+
+                draw();
+                counter = 0;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    if (snakepos[i] <= -55 * 9)
+                    {
+                        timer1.Stop();
+
+                        if (i + 1 == gamble)
+                        {
+                            MessageBox.Show("Snake " + (i + 1) + " wins!, you win ");
+                            money += gambleamount * 2;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Snake " + (i + 1) + " wins!, you lose");
+                        }
+                        gamble = 0;
+                        gambleamount = 0;
+
+                        if (money == 0)
+                        {
+                            MessageBox.Show("you are broke, you lose");
+                            Application.Exit();
+                        }
+                        for (int ii = 0; ii < 5; ii++)
+                        {
+                            snakepos[ii] = 0;
+                        }
+                        draw();
+                    }
+                }
+            }
+        }
+
+        private void draw()
+        {
+            paper.Clear(Color.Gray);
+
+            label3.Text = "geld: " + money;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int ii = 0; ii < 75; ii++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        if (ii % 2 != 1)
+                        {
+                            paper.FillRectangle(checker1, 750 + 10 * i, 50 + 10 * ii, 10, 10);
+                        }
+                        else
+                        {
+                            paper.FillRectangle(checker2, 750 + 10 * i, 50 + 10 * ii, 10, 10);
+                        }
+                    }
+                    else
+                    {
+                        if (ii % 2 != 0)
+                        {
+                            paper.FillRectangle(checker1, 750 + 10 * i, 50 + 10 * ii, 10, 10);
+                        }
+                        else
+                        {
+                            paper.FillRectangle(checker2, 750 + 10 * i, 50 + 10 * ii, 10, 10);
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int ii = 0; ii < 5; ii++)
+                {
+                    if (ii == 0)
+                    {
+                        paper.FillRectangle(
+                            head,
+                            250 - 55 * ii - snakepos[i],
+                            150 + 100 * i,
+                            50,
+                            50
+                        );
+                    }
+                    if (ii != 0)
+                    {
+                        paper.FillRectangle(
+                            body,
+                            250 - 55 * ii - snakepos[i],
+                            150 + 100 * i,
+                            50,
+                            50
+                        );
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (gamble == 0)
+            {
+                gamble = Convert.ToInt32(textBox1.Text);
+
+                if (Convert.ToInt32(textBox2.Text) <= money)
+                {
+                    gambleamount = Convert.ToInt32(textBox2.Text);
+                    money -= gambleamount;
+                }
+                else
+                {
+                    MessageBox.Show("broooke");
+                }
+
+                draw();
+            }
+            else
+            {
+                MessageBox.Show("holy addict you already gambled");
+            }
+        }
+    }
+}
