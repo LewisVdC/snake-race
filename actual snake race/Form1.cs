26a9f7e2bf0834515c6507d507b78ad6d9d4b981
Lewis Van den Cruyce <lewis.vandencruyce@leerling.kov.be>
2026-05-11 11:02:02 +0200

    n

--- actual snake race/Form1.cs
+++ actual snake race/Form1.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
 
 namespace actual_snake_race
 {
@@ -99,7 +100,6 @@ namespace actual_snake_race
         private void timer1_Tick(object sender, EventArgs e)
         {
             counter++;
-
             if (counter >= 50)
             {
                 int index = rand.Next(0, Convert.ToInt32(textBox3.Text));
@@ -226,6 +226,7 @@ namespace actual_snake_race
                 else
                 {
                     MessageBox.Show("broooke");
+                    gamble = 0;
                 }
             }
             else
